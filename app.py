4e2c3ede410d209ac1b152735b646f2514307ae0
Mariana Stefani <mariana.stefani89@gmail.com>
2020-03-31 08:57:25 +0000

    Created addtask.html and included a form

--- app.py
+++ app.py
@@ -18,9 +18,12 @@ def get_tasks():
     return render_template('tasks.html', tasks=mongo.db.tasks.find())
 
 
+@app.route('/add_task')
+def add_task():
+    return render_template('addtask.html')
 
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-    port=int(os.environ.get('PORT')),
-    debug=True)
+            port=int(os.environ.get('PORT')),
+            debug=True)
