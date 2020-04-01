32b6e9053b61fa850ffff2afebbaf32036b67d3f
Mariana Stefani <mariana.stefani89@gmail.com>
2020-04-01 14:19:57 +0000

    Fixed typo

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def update_task(task_id):
         'category_name': request.form.get('category_name'),
         'task_description': request.form.get('task_description'),
         'due_date': request.form.get('due_date'),
-        'is_urgent': request.form.det('is_urgent')
+        'is_urgent': request.form.get('is_urgent')
     })
     return redirect(url_for('get_tasks'))
 
